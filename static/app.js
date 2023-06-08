9cabfb296af245130ef098048b64864026301302
Anthony Pino <anthony.pino@outlook.fr>
2023-06-07 23:06:26 -0600

    add plot to js file to prove we can

--- static/app.js
+++ static/app.js
@@ -1,21 +1,10 @@
-// $.ajax({
-//   type:"GET",
-//   dataType: "json",
-//   url: "http://127.0.0.1:5000/api/v1.0/countries",
-//   success: beta
-// })
-       
-// function beta(data){
-//   buf1=data;
-//   console.log(data);
-// }
-
-
 // Define the URL of the JSON data
 var url = "http://127.0.0.1:5000/api/v1.0/countries";
+
 // Create an empty array to store the JSON data
-var data = [];
-// Use AJAX to get the JSON data from the URL
+let data = [];
+
+// Use AJAX to get the JSON data from the URL and make plots
 $.ajax({
   url: url,
   method: "GET",
@@ -25,10 +14,90 @@ $.ajax({
       // Add the JSON data to the array
       data.push(result[i]);
     }
+
+    // Since getting the JSON from flask was a success, plot what you need
+    var countries = data.map(item => item.country);
+    var happinessScores = data.map(item => item['happiness_score']);
+  
+    // Creating the trace for the bar graph
+    var trace = {
+    x: countries,
+    y: happinessScores,
+    type: 'bar',
+    marker: {
+      color: "green"
+    }
+    };
+  
+    // Combining the trace into an array
+    var graphData = [trace];
+  
+    // Layout configuration
+    var layout = {
+      title: 'Happiness Scores by Country',
+      xaxis: {
+        tickangle: -45
+      },
+      yaxis: { 
+        title: 'Happiness Score' 
+      },
+    };
+  
+    // Plotting the bar graph
+    Plotly.newPlot('plot', graphData, layout);
+  
   },
+
   error: function(error) {
     console.log(error);
   }
+  
 });
-console.log(data);
 
+// console.log(data);
+
+// var countries = data.map(item => item.country);
+// var happinessScores = data.map(item => item['happiness_score']);
+
+// // Creating the trace for the bar graph
+// var trace = {
+//   x: countries,
+//   y: happinessScores,
+//   type: 'bar'
+// };
+
+// // Combining the trace into an array
+// var graphData = [trace];
+
+// // Layout configuration
+// var layout = {
+//   title: 'Happiness Scores by Country',
+//   xaxis: { title: 'Country' },
+//   yaxis: { title: 'Happiness Score' }
+// };
+
+// // Plotting the bar graph
+// Plotly.newPlot('plot', graphData, layout);
+
+//   // Creating the trace for the bar graph
+//   var trace = {
+//     x: countries,
+//     y: happinessScores,
+//     type: 'bar'
+//   };
+
+//   // Combining the trace into an array
+//   var graphData = [trace];
+
+//   // Layout configuration
+//   var layout = {
+//     title: 'Happiness Scores by Country',
+//     xaxis: { title: 'Country' },
+//     yaxis: { title: 'Happiness Score' }
+//   };
+
+//   // Plotting the bar graph
+//   Plotly.newPlot('plot', graphData, layout);
+// } catch (error) {
+//   console.error('Error creating bar graph:', error);
+// }
